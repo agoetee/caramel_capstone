647ab7401dbc5f645cf38ab26888d86ff4cc6952
Charles Agoe <agoetee@gmail.com>
2024-05-01 13:08:43 +0000

    added base module and entries class

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class Entries(BaseModel):
+    date: None
+    time: None
+    text: str
+    no_of_calorie : float
 
 
 @app.get('/')
 def base():
     return {"info": "Hi there"}
+
+#Post request
+
+@app.post('/create')
+def create_entry(post: Entries):
+    """"""
