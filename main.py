8d1571e25bcbb63dbd0620c5fcfbc6d9935bfa56
Charles Agoe <agoetee@gmail.com>
2024-05-02 22:19:37 +0000

    #2 Added request body and POST

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from datetime import date, time
 
 app = FastAPI()
 
+# Entries Class from BaseModel
+
 class Entries(BaseModel):
     create_date: date
     create_time: time
     text: str
     no_of_calorie : float
 
+# GET request body
 
 @app.get('/')
 def base():
