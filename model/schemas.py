30a0e3cc18aa8bb53a86b25beff118cb7023d91f
Charles Agoe <agoetee@gmail.com>
2024-05-20 19:22:13 +0000

    #18 refactor and api-router path

---
+++ model/schemas.py
@@ -0,0 +1,51 @@
+"""
+The schemas file contains Pydantic models
+"""
+
+from pydantic import BaseModel
+from datetime import date, time
+from typing import Optional, List
+
+
+
+# Entries Class from BaseModel
+class EntriesBase(BaseModel):
+    id : int
+    create_date: date = None
+    create_time: time = None
+    text: str
+    no_of_calories : float
+
+
+class Entries(EntriesBase):
+    class Config:
+        from_attributes = True
+
+class UpdateEntries(BaseModel):
+    text: Optional[str] = None
+    no_of_calories : Optional[float] = None
+
+
+
+class UserBase(BaseModel):
+    id: int
+    name: str
+    username: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    username: str
+    email: str
+    entries: List[EntriesBase] = []
+
+    class Config:
+        from_attributes = True
+
+class ShowEntry(BaseModel):
+    text: str
+    no_of_calories: float
+    users: ShowUser
+
+    class Config:
+        from_attributes = True
