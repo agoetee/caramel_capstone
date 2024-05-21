8e683547fd8097a551d907ebe869634f72110e8f
Charles Agoe <agoetee@gmail.com>
2024-05-21 16:19:55 +0000

    #20 added aithentication module router

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from model import models
 from database import engine
-from routers import entries, user
+from routers import entries, user, authenticate
 
 app = FastAPI()
 
@@ -14,5 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 def base():
     return {"info": "Hi there"}
 
+app.include_router(authenticate.api_router)
 app.include_router(entries.api_router)
 app.include_router(user.api_router)
